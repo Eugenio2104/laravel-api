db311b93b4887b485a9cf8772711375df76ed291
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-31 16:26:34 +0100

    fatto model e migration

---
+++ resources/views/email/new-contact.blade.php
@@ -0,0 +1,10 @@
+<h1>nuovo messaggio per admin</h1>
+
+<p>
+    hai ricevuto un nuovo messaggio : <br>
+
+    Nome : {{ $lead->name }} <br>
+    email : {{ $lead->email }} <br>
+    messaggio : {{ $lead->message }} <br>
+
+</p>
