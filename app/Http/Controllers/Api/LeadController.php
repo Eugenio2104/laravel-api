ab700fb64e75a7d7c248df0486222d1c490b4073
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-31 18:25:59 +0100

    messaggio inviato a mail trap

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
 use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
@@ -44,6 +46,7 @@ class LeadController extends Controller
         $new_lead->fill($data);
         $new_lead->save();
 
+        Mail::to('eugenio@mail.com')->send(new NewContact($new_lead));
         return response()->json(compact('success'));
     }
 }
